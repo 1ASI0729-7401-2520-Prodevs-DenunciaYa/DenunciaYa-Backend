07376764497045d25c089799e5763b6f39c60d66
Franco Rioja <diegoriojanu@gmail.com>
2025-10-31 10:59:57 -0500

    initial commit

---
+++ src/main/java/com/denunciayabackend/DenunciaYaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.denunciayabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenunciaYaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DenunciaYaBackendApplication.class, args);
+    }
+
+}
