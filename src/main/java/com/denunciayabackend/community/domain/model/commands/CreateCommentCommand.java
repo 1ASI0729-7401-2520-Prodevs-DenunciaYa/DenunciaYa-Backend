c4d827f7ebe9dcdd6d0dd578fad08a4a9d3ff597
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-10 18:59:13 -0500

    feet: add create comment command

---
+++ src/main/java/com/denunciayabackend/community/domain/model/commands/CreateCommentCommand.java
@@ -0,0 +1,19 @@
+package com.denunciayabackend.community.domain.model.commands;
+
+public record CreateCommentCommand(Long postId, String userId, String author, String content) {
+
+    public CreateCommentCommand {
+        if (postId == null || postId <= 0) {
+            throw new IllegalArgumentException("postId must not be null or less than 1");
+        }
+        if (userId == null || userId.isBlank()) {
+            throw new IllegalArgumentException("userId must not be null or blank");
+        }
+        if (author == null || author.isBlank()) {
+            throw new IllegalArgumentException("author must not be null or blank");
+        }
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("content must not be null or blank");
+        }
+    }
+}
