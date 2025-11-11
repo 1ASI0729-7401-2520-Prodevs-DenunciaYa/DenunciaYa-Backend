cb4b2c22f19e04df87e1646a5e7ee3e1b81bb824
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-10 19:01:44 -0500

    feet: add Delete Post Command

---
+++ src/main/java/com/denunciayabackend/community/domain/model/commands/DeletePostCommand.java
@@ -0,0 +1,10 @@
+package com.denunciayabackend.community.domain.model.commands;
+
+public record DeletePostCommand(Long postId) {
+    public DeletePostCommand{
+        if(postId==null){
+            throw new IllegalArgumentException("courseId cannot be null ");
+
+        }
+    }
+}
