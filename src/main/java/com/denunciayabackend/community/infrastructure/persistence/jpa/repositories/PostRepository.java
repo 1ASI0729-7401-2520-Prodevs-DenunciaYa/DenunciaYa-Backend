a0531bcebac9637e20031be52eeb2615a121cb3f
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-10 20:11:28 -0500

    feat: add post repository

---
+++ src/main/java/com/denunciayabackend/community/infrastructure/persistence/jpa/repositories/PostRepository.java
@@ -0,0 +1,22 @@
+package com.denunciayabackend.community.infrastructure.persistence.jpa.repositories;
+
+import com.denunciayabackend.community.domain.model.aggregates.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByAuthor(String author);
+
+    List<Post> findByUserId(String userId);
+
+    List<Post> findByContentContainingIgnoreCase(String keyword);
+
+    boolean existsByIdAndAuthor(Long id, String author);
+
+    Optional<Post> findByImageUrl(String imageUrl);
+}
