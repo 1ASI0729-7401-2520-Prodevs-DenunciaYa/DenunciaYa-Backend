f642464718bb083ba9c872c15f4a85d4f250238b
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-10 19:20:28 -0500

    feat: add post command service

---
+++ src/main/java/com/denunciayabackend/community/domain/services/PostCommandService.java
@@ -0,0 +1,16 @@
+package com.denunciayabackend.community.domain.services;
+
+import com.denunciayabackend.community.domain.model.aggregates.Post;
+import com.denunciayabackend.community.domain.model.commands.CreateCommentCommand;
+import com.denunciayabackend.community.domain.model.commands.CreatePostCommand;
+import com.denunciayabackend.community.domain.model.commands.DeletePostCommand;
+import com.denunciayabackend.community.domain.model.commands.LikePostCommand;
+
+import java.util.Optional;
+
+public interface PostCommandService {
+    Long handle(CreatePostCommand command);
+    void handle(DeletePostCommand command);
+    Optional<Post> handle(LikePostCommand command);
+    Optional<Post> handle(CreateCommentCommand command);
+}
