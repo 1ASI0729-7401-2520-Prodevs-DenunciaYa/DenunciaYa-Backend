9d0317d7e7fb381c2bad05d7ac33535a4ef05dae
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-16 15:18:17 -0500

    update:add docker file

--- src/main/java/com/denunciayabackend/authoritiesPanel/domain/model/commands/DeleteResponsibleCommand.java
+++ src/main/java/com/denunciayabackend/authoritiesPanel/domain/model/commands/DeleteResponsibleCommand.java
@@ -6,7 +6,7 @@ public record DeleteResponsibleCommand(Long responsibleId) {
     public DeleteResponsibleCommand {
         if (responsibleId == null || responsibleId < 1) {
             throw new IllegalArgumentException("ResponsibleId " +
-                    "cannot be null or less than 1.");
+                    "cannot be null or less than 1");
         }
     }
     public ResponsibleId toValueObject() {
