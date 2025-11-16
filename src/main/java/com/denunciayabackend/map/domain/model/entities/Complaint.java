26a9a92a4984d998b196e878aaaa271ae1917cdc
Franco Rioja <131830557+FrancoDiegoRioja@users.noreply.github.com>
2025-11-16 10:00:32 -0500

    feat(map): Define domain model, query, and API resources for map BC

---
+++ src/main/java/com/denunciayabackend/map/domain/model/entities/Complaint.java
@@ -0,0 +1,39 @@
+package com.denunciayabackend.map.domain.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "complaints")
+public class Complaint {
+
+    @Id
+    private String id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "district")
+    private String district;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "latitude")
+    private double latitude;
+
+    @Column(name = "longitude")
+    private double longitude;
+
+}
