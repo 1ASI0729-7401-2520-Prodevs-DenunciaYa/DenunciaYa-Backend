98122a5e11069130d1887cf4e2770a620e1a1df6
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-23 18:21:22 -0500

    update:fix application properties

--- src/main/java/com/denunciayabackend/DenunciaYaBackendApplication.java
+++ src/main/java/com/denunciayabackend/DenunciaYaBackendApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DenunciaYaBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DenunciaYaBackendApplication.class, args);
     }
+
 }
