b3e7de2e9063d19411d47f770879b361eabb7e11
Gabriel Mamani <u202220659@upc.edu.pe>
2025-11-23 19:21:46 -0500

    update:fix comment

--- src/main/java/com/denunciayabackend/community/interfaces/rest/transform/CommentResourceFromEntityAssembler.java
+++ src/main/java/com/denunciayabackend/community/interfaces/rest/transform/CommentResourceFromEntityAssembler.java
@@ -3,15 +3,20 @@ package com.denunciayabackend.community.interfaces.rest.transform;
 import com.denunciayabackend.community.domain.model.entities.Comment;
 import com.denunciayabackend.community.interfaces.rest.resources.CommentResource;
 
+import java.util.Date;
+import java.time.ZoneId;
+
 public class CommentResourceFromEntityAssembler {
 
     public static CommentResource toResourceFromEntity(Comment entity) {
+        Date createdAt = Date.from(entity.getCreatedAt().atZone(ZoneId.systemDefault()).toInstant());
+
         return new CommentResource(
                 entity.getId(),
                 entity.getUserId().userId(),
                 entity.getAuthor(),
                 entity.getContent(),
-                entity.getCreatedAt()
+                createdAt
         );
     }
 }
