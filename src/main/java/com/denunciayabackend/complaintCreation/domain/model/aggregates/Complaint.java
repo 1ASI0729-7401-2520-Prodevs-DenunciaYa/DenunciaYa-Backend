d9b20586d29ac76b676a9152ba4c9e84464c0e2c
TicSide <66962537+TicSide@users.noreply.github.com>
2025-12-01 10:34:32 -0500

    Update Complaint.java

--- src/main/java/com/denunciayabackend/complaintCreation/domain/model/aggregates/Complaint.java
+++ src/main/java/com/denunciayabackend/complaintCreation/domain/model/aggregates/Complaint.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "ComplaintCreation")
-@Table(name = "complaint_creation")
+@Table(name = "complaints")
 public class Complaint extends AuditableAbstractAggregateRoot<Complaint> {
 
     @Id
