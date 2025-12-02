d1ad18d7bb5ce5771bd33f54365347f9d4b7efec
Franco Rioja <131830557+FrancoDiegoRioja@users.noreply.github.com>
2025-12-02 18:40:23 -0500

    fix(authoritiesPanel): Fix bug preventing deletion of responsibles with valid IDs

--- src/main/java/com/denunciayabackend/authoritiesPanel/domain/model/commands/DeleteResponsibleCommand.java
+++ src/main/java/com/denunciayabackend/authoritiesPanel/domain/model/commands/DeleteResponsibleCommand.java
@@ -1,13 +1,9 @@
 package com.denunciayabackend.authoritiesPanel.domain.model.commands;
 
-import com.denunciayabackend.authoritiesPanel.domain.model.valueobjects.ResponsibleId;
-
 public record DeleteResponsibleCommand(Long responsibleId) {
     public DeleteResponsibleCommand {
-        if (responsibleId == null || responsibleId>1) {
-            throw new IllegalArgumentException("ResponsibleId " +
-                    "cannot be null or less than 1.");
+        if (responsibleId == null || responsibleId < 1) {
+            throw new IllegalArgumentException("ResponsibleId cannot be null or less than 1.");
         }
     }
-
 }
