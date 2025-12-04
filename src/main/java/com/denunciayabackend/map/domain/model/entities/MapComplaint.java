a2231616bcad5b1bfd4e6aafa2ae25acbc2bb86b
Franco Rioja <131830557+FrancoDiegoRioja@users.noreply.github.com>
2025-12-04 14:58:23 -0500

    fix(map): MAP FIXED

--- src/main/java/com/denunciayabackend/map/domain/model/entities/MapComplaint.java
+++ src/main/java/com/denunciayabackend/map/domain/model/entities/MapComplaint.java
@@ -3,34 +3,38 @@ package com.denunciayabackend.map.domain.model.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Immutable;
+import org.hibernate.annotations.Subselect;
 
 @Getter
 @NoArgsConstructor
 @Entity(name = "MapComplaint")
-@Table(name = "map_complaint")
 @Immutable
+
+@Subselect(
+        "SELECT " +
+                "  c.complaint_id as id, " +
+                "  c.description as title, " +
+                "  c.category as category, " +
+                "  c.district as district, " +
+                "  c.status as status, " +
+                "  c.location as location " +
+                "FROM complaints c"
+)
 public class MapComplaint {
 
     @Id
-    @Column(name = "complaint_id")
     private String id;
 
-    @Column(name = "description")
     private String title;
 
-    @Column(name = "category")
     private String category;
 
-    @Column(name = "district")
     private String district;
 
-    @Column(name = "status")
     private String status;
 
-    @Column(name = "location")
     private String location;
 }
