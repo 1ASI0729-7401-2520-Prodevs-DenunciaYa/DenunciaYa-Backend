40fbbc0face67864b105e768380ab0face89c79b
Franco Rioja <131830557+FrancoDiegoRioja@users.noreply.github.com>
2025-12-03 21:39:45 -0500

    fix(map): Missing variables were added

--- src/main/java/com/denunciayabackend/map/interfaces/resources/ComplaintMapResource.java
+++ src/main/java/com/denunciayabackend/map/interfaces/resources/ComplaintMapResource.java
@@ -4,6 +4,7 @@ public record ComplaintMapResource(
         String id,
         String title,
         String category,
+        String district,
         String status,
         double latitude,
         double longitude
