00d28d57dc2109747c3206be0cdc07148d52cb24
HaroldTicllacuri <66962537+TicSide@users.noreply.github.com>
2025-12-04 21:52:00 -0500

    Update WebSecurityConfiguration.java

--- src/main/java/com/denunciayabackend/iam/infraestructure/authorization/sfs/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/denunciayabackend/iam/infraestructure/authorization/sfs/configuration/WebSecurityConfiguration.java
@@ -81,7 +81,11 @@ public class  WebSecurityConfiguration {
                 "/swagger-ui/**",
                 "/swagger-resources/**",
                 "/webjars/**",
-                "/uploads/**"
+                "/uploads/**",
+
+                "/api/v1/map/complaints",
+                "/api/v1/responsibles",
+                "/api/v1/complaintAssignments/**"
         };
 
         // --- CORS FIX REAL ---
